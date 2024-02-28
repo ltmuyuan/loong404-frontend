91a252ef6139387b0c25e839f47622b018b69cf1
brab <frontend_brab@gmail.com>
2024-02-28 22:50:14 +0800

    feat: update token swap

--- src/components/tokenswap.jsx
+++ src/components/tokenswap.jsx
@@ -150,7 +150,8 @@ const TokenSwap = () => {
                 await swap(walletProvider, firstInput, firstValue === 1)
                 message.success('Swap success')
             } catch (e) {
-                message.error('Swap failed')
+                const msg = e.message ? `Swap failed: ${e.message}` : 'Swap failed'
+                message.error(msg)
             }
         }
     }
--- src/utils/web3.js
+++ src/utils/web3.js
@@ -3,18 +3,37 @@ import GreatLABI from "./abi/GreatL.json";
 import BabyLABI from "./abi/BabyL.json";
 import DataABI from "./abi/IDataStorage.json";
 import { ethers } from "ethers";
+import { message } from "antd";
 
+/** 
+ * those data only use in test environment
+ */
 const GreatLoongAddress = "0x512273384A35D749207Db806cF3E1ed89E5613a3"
 const BabyLoongAddress = "0xafA0860737Fa278812e7374681bC83bc1b031F51"
 const swapAddress = "0x936201bc8eecc9F062b938a9B40Ea25133513d99"
 const dataAddress = "0x9514C7DB087458fE82F1f93cFe4229EB38d88F38"
 
-export const swap = async (walletProvider, amount, isBig) => {
+/**
+ * Swap tokens
+ * @param {* this is a browser provider ethers can use} walletProvider 
+ * @param {* this is a number or string you need to swap} amount 
+ * @param {* boolean check is or not Great to Baby} isGreat 
+ */
+export const swap = async (walletProvider, amount, isGreat) => {
     const ethersProvider = new ethers.BrowserProvider(walletProvider)
     const signer = await ethersProvider.getSigner()
     const contract = new ethers.Contract(swapAddress, SwapABI.abi, signer)
     const value = ethers.parseEther(amount.toString())
-    if (isBig) {
+    let balance = 0
+    if (isGreat) {
+        balance = await getBalanceGreatLoong(walletProvider)
+    } else {
+        balance = await getBalanceBabyLoong(walletProvider)
+    }
+    if (balance < amount) {
+        throw new Error('Insufficient balance')
+    }
+    if (isGreat) {
         const tx = await contract.swap(GreatLoongAddress, BabyLoongAddress, value.toString())
         await tx.wait()
     } else {
@@ -23,6 +42,11 @@ export const swap = async (walletProvider, amount, isBig) => {
     }
 }
 
+/**
+ * Get GreatLoong balance
+ * @param {* this is a browser provider ethers can use} walletProvider 
+ * @returns amount of GreatLoong
+ */
 export const getBalanceGreatLoong = async (walletProvider) => {
     const ethersProvider = new ethers.BrowserProvider(walletProvider)
     const signer = await ethersProvider.getSigner()
@@ -31,6 +55,11 @@ export const getBalanceGreatLoong = async (walletProvider) => {
     return balance.toString()
 }
 
+/**
+ * Get BabyLoong balance
+ * @param {* this is a browser provider ethers can use} walletProvider
+ * @returns amount of BabyLoong
+ */
 export const getBalanceBabyLoong = async (walletProvider) => {
     const ethersProvider = new ethers.BrowserProvider(walletProvider)
     const signer = await ethersProvider.getSigner()
@@ -39,6 +68,11 @@ export const getBalanceBabyLoong = async (walletProvider) => {
     return balance.toString()
 }
 
+/**
+ * Get invite code
+ * @param {* this is a browser provider ethers can use} walletProvider
+ * @returns invite code
+ */
 export const getInviteCode = async (walletProvider) => {
     const ethersProvider = new ethers.BrowserProvider(walletProvider)
     const signer = await ethersProvider.getSigner()
@@ -47,6 +81,11 @@ export const getInviteCode = async (walletProvider) => {
     return inviteCode
 }
 
+/**
+ * Generate invite code
+ * @param {* this is a browser provider ethers can use} walletProvider
+ * @returns invite code
+ */
 export const generateInviteCode = async (walletProvider) => {
     const ethersProvider = new ethers.BrowserProvider(walletProvider)
     const signer = await ethersProvider.getSigner()
@@ -64,6 +103,11 @@ export const generateInviteCode = async (walletProvider) => {
     return code
 }
 
+/**
+ * Get invite codes
+ * @param {* this is a browser provider ethers can use} walletProvider
+ * @returns invite codes
+ */
 export const getInviteCodes = async (walletProvider) => {
     const ethersProvider = new ethers.BrowserProvider(walletProvider)
     const signer = await ethersProvider.getSigner()
@@ -72,6 +116,10 @@ export const getInviteCodes = async (walletProvider) => {
     return codes
 }
 
+/**
+ * Create a 6-letter invite code
+ * @returns 6-letter invite code
+ */
 export const createSixLetterNum = () => {
     let result = '';
     const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
@@ -80,4 +128,43 @@ export const createSixLetterNum = () => {
         result += characters.charAt(Math.floor(Math.random() * charactersLength));
     }
     return result;
+}
+
+/**
+ * Get discount list
+ * @param {* this is a browser provider ethers can use} walletProvider
+ * @returns {* boolean is or not white list} isWhiteList
+ */
+export const getDiscountList = async (walletProvider) => {
+    const ethersProvider = new ethers.BrowserProvider(walletProvider)
+    const signer = await ethersProvider.getSigner()
+    const contract = new ethers.Contract(dataAddress, DataABI.abi, signer)
+    const num = await contract.getDiscountList(signer.getAddress())
+    return num > 0
+}
+
+/**
+ * get free greatL amount
+ * @param {* this is a browser provider ethers can use} walletProvider
+ * @returns amount of adult L
+ */
+export const buyListAdultL = async (walletProvider) => {
+    const ethersProvider = new ethers.BrowserProvider(walletProvider)
+    const signer = await ethersProvider.getSigner()
+    const contract = new ethers.Contract(dataAddress, DataABI.abi, signer)
+    const num = await contract.buyListAdultL(signer.getAddress())
+    return num
+}
+
+/**
+ * get free babyL amount
+ * @param {* this is a browser provider ethers can use} walletProvider
+ * @returns amount of baby L
+ */
+export const buyListBabyL = async (walletProvider) => {
+    const ethersProvider = new ethers.BrowserProvider(walletProvider)
+    const signer = await ethersProvider.getSigner()
+    const contract = new ethers.Contract(dataAddress, DataABI.abi, signer)
+    const num = await contract.buyListBabyL(signer.getAddress())
+    return num
 }
