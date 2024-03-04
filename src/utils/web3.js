32d2238d7ec87fbacd130a99364677c2356c9d11
jerikchan <jerikchan@gmail.com>
2024-03-04 11:07:38 +0800

    fix: ts error

--- src/utils/web3.js
+++ src/utils/web3.js
@@ -224,7 +224,6 @@ export const getClaimAmount = async (walletProvider, isGreateL) => {
     const contract = new ethers.Contract(isGreateL ? greatLMintAddr : babyLMintAddr, MintABI.abi, signer)
     const num = await contract.getClaimAmount(signer.getAddress())
     const numReal = ethers.formatEther(num)
-    debugger;
     return numReal.toString()
 }
 
