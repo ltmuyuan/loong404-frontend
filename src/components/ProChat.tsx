c72d50fe5690c6525891af03176c49bc3e10738e
jerikchan <jerikchan@gmail.com>
2024-03-08 15:26:38 +0800

    feat: fe limit nft

--- src/components/ProChat.tsx
+++ src/components/ProChat.tsx
@@ -1,6 +1,6 @@
 import { ProChat as ProChatAntd } from "@ant-design/pro-chat";
 
-const ProChat = (props: any) => {
+const ProChat = (props: React.ComponentProps<typeof ProChatAntd>) => {
   return <ProChatAntd {...props}></ProChatAntd>
 }
 
